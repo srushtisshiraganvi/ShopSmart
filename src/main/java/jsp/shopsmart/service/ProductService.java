dc01437a19d0a27c74087ce0181ea99957423201
srushtisshiraganvi <srushtishiragavi@gmail.com>
2025-10-26 20:26:13 +0530

    completed

--- src/main/java/jsp/shopsmart/service/ProductService.java
+++ src/main/java/jsp/shopsmart/service/ProductService.java
@@ -3,7 +3,6 @@ package jsp.shopsmart.service;
 import jsp.shopsmart.model.Product;
 import jsp.shopsmart.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
